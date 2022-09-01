2d97a5cba8fdb1d7105da09c334a696cc1f122d9
mauro <mauro@genuino.world>
2022-09-01 12:21:26 +0200

    Tested addGuestThroughMailChimp.

---
+++ server/src/entitites/guest/addGuestThroughMailChimp.test.ts
@@ -0,0 +1,57 @@
+import { addGuestThroughMailChimp } from './addGuestThroughMailChimp'
+import { MD5 } from 'crypto-js'
+import { expectResult } from '../../../../shared/testUtils'
+import { ObjectId } from 'mongodb'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const getListMember = jest.fn((_listId: string, _emailHash: string) =>
+  Promise.resolve({
+    status: 'subscribed',
+    email_address: 'email.address@example.com',
+    merge_fields: {
+      FNAME: 'First name',
+      LNAME: 'Last name',
+      MMERGE8: 'Company name',
+    },
+  }),
+)
+
+jest.mock('../../resources/mailchimp', () => ({
+  mailchimp: {
+    use: jest.fn(
+      <T>(op: (guest: unknown) => Promise<T>): Promise<T> =>
+        op({
+          lists: {
+            getListMember,
+          },
+        }),
+    ),
+  },
+}))
+
+describe('addGuestThroughMailChimp', () => {
+  it('should work', async () => {
+    const result = await addGuestThroughMailChimp({
+      params: {
+        listId: 'listId',
+        email: 'email',
+      },
+      query: {},
+      body: {},
+    })
+
+    expect(getListMember).toHaveBeenCalledTimes(1)
+    expect(getListMember).toHaveBeenCalledWith(
+      'listId',
+      MD5('email').toString(),
+    )
+
+    expectResult(result).toHaveSucceededWith({
+      _id: expect.any(ObjectId),
+      firstName: 'First name',
+      lastName: 'Last name',
+      email: 'email.address@example.com',
+      companyName: 'Company name',
+    })
+  })
+})
