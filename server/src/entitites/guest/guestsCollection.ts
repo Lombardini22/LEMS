684b29a96fddeb5dfafe99c5418c4fd3ac29690d
mauro <mauro@genuino.world>
2022-09-06 13:44:19 +0200

    Skipping index creation when testing.

--- server/src/entitites/guest/guestsCollection.ts
+++ server/src/entitites/guest/guestsCollection.ts
@@ -2,23 +2,28 @@ import { Collection } from '../../database/Collection'
 import { Guest } from '../../../../shared/models/Guest'
 import { Result } from '../../../../shared/Result'
 import { ServerError } from '../../ServerError'
+import { constVoid } from '../../../../shared/utils'
 
-export const guestsCollection = new Collection<Guest>('guests', collection =>
-  Result.tryCatch(
-    () => {
-      collection.createIndexes([
-        {
-          key: {
-            emailHash: 1,
+export const guestsCollection = new Collection<Guest>('guests', collection => {
+  if (process.env['NODE_ENV'] === 'test') {
+    return Result.success(constVoid)
+  } else {
+    return Result.tryCatch(
+      () => {
+        collection.createIndexes([
+          {
+            key: {
+              emailHash: 1,
+            },
+            unique: true,
+            name: 'uniqueEmailHash',
           },
-          unique: true,
-          name: 'uniqueEmailHash',
-        },
-      ])
-    },
-    error =>
-      new ServerError(500, 'Unable to create indexes on guests collection', {
-        error,
-      }),
-  ),
-)
+        ])
+      },
+      error =>
+        new ServerError(500, 'Unable to create indexes on guests collection', {
+          error,
+        }),
+    )
+  }
+})
