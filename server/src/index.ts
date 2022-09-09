a483a14249beee769733c1b36e0f807807053516
mauro <mauro@genuino.world>
2022-09-09 12:52:31 +0200

    Testing suspected double build.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,14 @@
 import { Result } from '../../shared/Result'
 import { guestsRouter } from './entitites/guest/guestsRouter'
+import { env } from './resources/env'
 import { Server } from './resources/Server'
 
-Server.make()
-  .withRouter(guestsRouter)
-  .use(() => Result.success(() => console.log('Server is ready')))
+env.use(env =>
+  Server.make()
+    .withRouter(guestsRouter)
+    .use(() =>
+      Result.success(() =>
+        console.log(`Server is listening to port ${env.PORT}`),
+      ),
+    ),
+)
