345d0400e64046be9a5793f964bf999914a3a91b
mauro <mauro@genuino.world>
2022-09-12 13:10:38 +0200

    Subscribing RSVP and referrees to new list.

--- server/src/entitites/guest/createGuest.ts
+++ server/src/entitites/guest/createGuest.ts
@@ -9,6 +9,7 @@ import { Path } from '../../routing/Path'
 import { Request } from '../../routing/Router'
 import { ServerError } from '../../ServerError'
 import { guestsCollection } from './guestsCollection'
+import { subscribeGuest } from './utils/subscribeGuest'
 
 type CreateGuestQuery = {
   referrerEmail?: string
@@ -22,10 +23,38 @@ export async function createGuest(
   const guestEmailHash = hashGuestEmail(req.body.email)
   const referrerEmail = req.query.referrerEmail
 
-  if (referrerEmail) {
-    const referrer = await guestsCollection.findOne({ email: referrerEmail })
+  const guest = await (async () => {
+    if (referrerEmail) {
+      const referrer = await guestsCollection.findOne({ email: referrerEmail })
 
-    return referrer.flatMap(async referrer => {
+      return referrer.flatMap(async referrer => {
+        const guest = await guestsCollection.findOne({
+          emailHash: guestEmailHash,
+        })
+
+        if (guest.isSuccess()) {
+          return guestsCollection.update(
+            { emailHash: guestEmailHash },
+            {
+              ...guest.unsafeGetValue(),
+              ...req.body,
+              source: 'REFERRER',
+              referrerId: referrer._id,
+            },
+          )
+        } else {
+          const guest = await guestsCollection.insert({
+            ...req.body,
+            emailHash: guestEmailHash,
+            source: 'REFERRER',
+            referrerId: referrer._id,
+            status: 'RSVP',
+          })
+
+          return guest.flatMap(guest => subscribeGuest(guest))
+        }
+      })
+    } else {
       const guest = await guestsCollection.findOne({
         emailHash: guestEmailHash,
       })
@@ -36,38 +65,18 @@ export async function createGuest(
           {
             ...guest.unsafeGetValue(),
             ...req.body,
-            source: 'REFERRER',
-            referrerId: referrer._id,
           },
         )
       } else {
         return guestsCollection.insert({
           ...req.body,
           emailHash: guestEmailHash,
-          source: 'REFERRER',
-          referrerId: referrer._id,
+          source: 'MANUAL',
           status: 'RSVP',
         })
       }
-    })
-  } else {
-    const guest = await guestsCollection.findOne({ emailHash: guestEmailHash })
-
-    if (guest.isSuccess()) {
-      return guestsCollection.update(
-        { emailHash: guestEmailHash },
-        {
-          ...guest.unsafeGetValue(),
-          ...req.body,
-        },
-      )
-    } else {
-      return guestsCollection.insert({
-        ...req.body,
-        emailHash: guestEmailHash,
-        source: 'MANUAL',
-        status: 'RSVP',
-      })
     }
-  }
+  })()
+
+  return guest
 }
