3caa5d919c32d6f116a8e17cc76e590d175c5540
mauro <mauro@genuino.world>
2022-09-20 11:55:21 +0200

    Always inserting on MailChimp first.

--- server/src/entitites/guest/uploadGuests.ts
+++ server/src/entitites/guest/uploadGuests.ts
@@ -24,44 +24,48 @@ export function uploadGuests(
   if (data.success) {
     return env.use(env =>
       mailchimp.use(async mailchimp => {
-        const localResult = await guestsCollection.insert(
-          data.data.map(
-            (guest): NoTimestamps<Guest> => ({
-              ...guest,
-              companyName: guest.companyName || null,
-              emailHash: hashGuestEmail(guest.email),
-              source: 'UPLOAD',
-              status: 'RSVP',
-              accountManager: null,
-            }),
-          ),
-        )
-
-        const mailchimpResult = await localResult.flatMap(() =>
-          Result.tryCatch(
-            () =>
-              // Not typed in MailChimp lib, but this exists
-              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-              // @ts-ignore
-              mailchimp.lists.batchListMembers(env.MAILCHIMP_EVENT_LIST_ID, {
-                members: data.data.map(guest =>
-                  guestToMailchimpListMember({
-                    ...guest,
-                    companyName: guest.companyName || null,
-                  }),
-                ),
-                update_existing: true,
-              }),
-            error =>
-              new ServerError(
-                500,
-                'Unable to get current guests from MailChimp API',
-                { error },
+        const mailchimpResult = await Result.tryCatch(
+          () =>
+            // Not typed in MailChimp lib, but this exists
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            mailchimp.lists.batchListMembers(env.MAILCHIMP_EVENT_LIST_ID, {
+              members: data.data.map(guest =>
+                guestToMailchimpListMember({
+                  ...guest,
+                  emailHash: hashGuestEmail(guest.email),
+                  companyName: guest.companyName || null,
+                }),
               ),
-          ),
+              update_existing: true,
+            }),
+          error =>
+            new ServerError(
+              500,
+              'Unable to get current guests from MailChimp API',
+              { error },
+            ),
         )
 
-        return mailchimpResult.map(response => ({
+        const mailchimpResultAfterLocalInsertion =
+          await mailchimpResult.flatMap(async mailchimpResponse => {
+            const insertResult = await guestsCollection.insert(
+              data.data.map(
+                (guest): NoTimestamps<Guest> => ({
+                  ...guest,
+                  companyName: guest.companyName || null,
+                  emailHash: hashGuestEmail(guest.email),
+                  source: 'UPLOAD',
+                  status: 'RSVP',
+                  accountManager: null,
+                }),
+              ),
+            )
+
+            return insertResult.map(() => mailchimpResponse)
+          })
+
+        return mailchimpResultAfterLocalInsertion.map(response => ({
           processedCount: data.data.length,
           uploadedCount: response.total_created + response.total_updated,
           createdCount: response.total_created,
