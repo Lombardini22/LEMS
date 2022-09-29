d2036205552a466a2a24f7513e11cb1d4ed0dbe2
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2022-09-29 12:50:27 +0200

    Added previousStatus to checkInGuest result.

--- server/src/entitites/guest/checkInGuest.ts
+++ server/src/entitites/guest/checkInGuest.ts
@@ -14,22 +14,33 @@ export const checkInGuestPath = Path.start()
   .param<CheckInGuestParams>('emailHash')
   .literal('check-in')
 
+type CheckedInGuest = WithId<Guest> & {
+  previousStatus: Guest['status']
+}
+
 export async function checkInGuest(
   req: Request<CheckInGuestParams, unknown, unknown>,
-): Promise<Result<ServerError, WithId<Guest>>> {
+): Promise<Result<ServerError, CheckedInGuest>> {
   const guest = await guestsCollection.findOne({
     emailHash: req.params.emailHash,
   })
 
-  const updateResult = await guest.flatMap(guest =>
-    guestsCollection.update(
+  const updateResult = await guest.flatMap(async guest => {
+    const previousStatus = guest.status
+
+    const result = await guestsCollection.update(
       { _id: guest._id },
       {
         ...guest,
         status: 'CHECKED_IN',
       },
-    ),
-  )
+    )
+
+    return result.map(result => ({
+      ...result,
+      previousStatus,
+    }))
+  })
 
   return updateResult.mapError(error => {
     if (error.status === 404) {
