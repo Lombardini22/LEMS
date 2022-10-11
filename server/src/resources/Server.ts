08ffc98f5bd8c9dc45a3ff486f72309be7146609
mauro <mauro@genuino.world>
2022-10-11 13:03:58 +0200

    Trying with origin: *.

--- server/src/resources/Server.ts
+++ server/src/resources/Server.ts
@@ -17,7 +17,7 @@ export class Server extends Resource<express.Express> {
       const app = express()
         .use(express.json())
         .use(express.urlencoded({ extended: true }))
-        .use(cors())
+        .use(cors({ origin: '*' }))
 
       const apiRouter = this.routers
         .reduce(
